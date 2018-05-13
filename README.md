733930b2f4226c31e4d1c08e27f1e42e3af93c01
Roman Pushkov <pushkovroman@me.com>
2018-05-13 20:12:15 +0300

    description update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Groundhog
 
-This is a simple application for running scheduled daily tasks
-Simply wrap your task in a zero-arity anonymous function and Bob's your uncle
-Example: Groundhog.schedule(fn -> DateTime.utc_now end)
+This is a simple application for running scheduled daily tasks.
+
+Simply wrap your task in a zero-arity anonymous function and Bob's your uncle!
+
+Example:
+```Groundhog.schedule(fn -> DateTime.utc_now end)```
 
 ## Installation
 
--- lib/groundhog.ex
+++ lib/groundhog.ex
@@ -1,8 +1,11 @@
 defmodule Groundhog do
   @moduledoc """
-  This is a simple application for running scheduled daily tasks
-  Simply wrap your task in a zero-arity anonymous function and Bob's your uncle
-  Example: Groundhog.schedule(fn -> DateTime.utc_now end)
+  This is a simple application for running scheduled daily tasks.
+  
+  Simply wrap your task in a zero-arity anonymous function and Bob's your uncle!
+  
+  Example:
+  ```Groundhog.schedule(fn -> DateTime.utc_now end)```
   """
 
   def schedule(task) when is_function(task, 0) do
